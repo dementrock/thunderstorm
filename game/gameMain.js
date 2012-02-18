5d6bce1fc899378bc3e6f859d0f2126c3bc958c6
Thanh Hai Mai <thanhhai.mai@berkeley.edu>
2012-02-18 00:31:51 -0800

    - working now

--- game/gameMain.js
+++ game/gameMain.js
@@ -7,6 +7,19 @@ var started = false;
 
 module.exports = function(app) {
   var game = new Game();
+
+  // var ship_a = new Ship([0, 0], "player");
+  // var bullet_a = new Bullet([10, 0], 1, [10, 0]);
+  // var bullet_b = new Bullet([0, 10], 1, [0, -1]);
+  // var bullet_c = new Bullet([0, 0], 100, [1, 0]);
+  // var bullet_d = new Bullet([0, 100], 100, [0, -1]);
+// 
+  // game.addShip(ship_a);
+  // game.addBullet(bullet_a);
+  // game.addBullet(bullet_b);
+  // game.addBullet(bullet_c);
+  // game.addBullet(bullet_d);
+
   var everyone = nowjs.initialize(app);
   var clients = [];
 
@@ -30,12 +43,14 @@ module.exports = function(app) {
   var onFrameUpdate = function(timeElapsed) {
   };
   var onStepUpdate = function(timeElapsed) {
-    if (started)
-      everyone.now.OnRender(JSON.stringify(game.ships));
+    if(started) {
+      everyone.now.OnRender(JSON.stringify(game.ships), JSON.stringify(game.bullets));
+    }
   };
+
   var onRender = function() {
   };
-  
+
   nowjs.on('connect', function() {
     started = true;
     clients[this.user.clientId] = {
@@ -52,6 +67,5 @@ module.exports = function(app) {
       }
     }
   });
-
   require('./gameloop')(onFrameUpdate, onStepUpdate, onRender);
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,74 +1,70 @@
 var ctx;
-now.ready(function () {
+now.ready(function() {
 })
 
-$(document).ready(function(){
-    var canvas = document.createElement("canvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = 800;
-    canvas.height = 600;
-    document.body.appendChild(canvas);
+$(document).ready(function() {
+  var canvas = document.createElement("canvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 600;
+  document.body.appendChild(canvas);
 
-    $(window).keydown(keyDown);
+  $(window).keydown(keyDown);
 });
-
 var keyDown = function(key) {
-    if (key == 38 || key == 87) { //up
-        now.moveUp({});
-    } else if (key == 40 || key == 83) { //down
-        now.moveDown({});
-    } else if (key == 37 || key == 65) { //left
-        now.moveLeft({});
-    } else if (key == 39 || key == 68) { //right
-        now.moveRight({});
-    }
+  if(key == 38 || key == 87) {//up
+    now.moveUp({});
+  } else if(key == 40 || key == 83) {//down
+    now.moveDown({});
+  } else if(key == 37 || key == 65) {//left
+    now.moveLeft({});
+  } else if(key == 39 || key == 68) {//right
+    now.moveRight({});
+  }
 }
-
-
 function drawBG() {
-    ctx.fillStyle = '#000'; //black
-    ctx.beginPath();
-    ctx.rect(0, 0, 800, 600); 
-    ctx.closePath();
-    ctx.fill();
+  ctx.fillStyle = '#000';
+  //black
+  ctx.beginPath();
+  ctx.rect(0, 0, 800, 600);
+  ctx.closePath();
+  ctx.fill();
 }
-
 
 function drawShip(ship) {
-    ctx.fillStyle = '#FFF'; //white
-    ctx.beginPath();
-    ctx.arc(ship.position[0], ship.position[1], ship.radius, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
+  ctx.fillStyle = '#FFF';
+  //white
+  ctx.beginPath();
+  ctx.arc(ship.position[0], ship.position[1], ship.radius, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
 }
 
 function drawBullet(bullet) {
-    ctx.fillStyle = '#F00';
-    ctx.beginPath();
-    ctx.arc(bullet.position[0], bullet.position[1], bullet.radius, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
-    
+  ctx.fillStyle = '#F00';
+  ctx.beginPath();
+  ctx.arc(bullet.position[0], bullet.position[1], bullet.radius, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+
 }
 
+now.OnRender = function(_ships, _bullets) {
+  var ships = JSON.parse(_ships);
+  var bullets = JSON.parse(_bullets);
 
-now.OnRender = function (gameState) {
-  var game = JSON.parse(gameState);
-  //game.ships
-  //game.bullets
-
-    drawBG();
-    for (var shipIndex in game.ships) {
-        var ship = this.ships[shipIndex];
-        if (ship.isAlive) {
-            drawShip(ship);
-        }
+  drawBG();
+  for(var shipIndex in ships) {
+    var ship = ships[shipIndex];
+    if(ship.isAlive) {
+      drawShip(ship);
     }
+  }
 
-    for (var bulletIndex in game.bullets) {
-        var bullet = game.bullets[bulletIndex];
-        if (bullet.isAlive) {
-            drawBullet(bullet);
-        }
+  for(var bulletIndex in bullets) {
+    var bullet = bullets[bulletIndex];
+    if(bullet.isAlive) {
+      drawBullet(bullet);
     }
+  }
 }
