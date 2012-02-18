68546dc33d7d8ebae5bd7ee4f653d25efeb7f6ff
Rocky Duan <dementrock@gmail.com>
2012-02-18 03:25:57 -0800

    waterwave not working

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,7 @@ now.ready(function() {
 
 $(document).ready(function() {
   var canvas = document.createElement("canvas");
+  canvas.setAttribute("id", "canvas");
   ctx = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 600;
