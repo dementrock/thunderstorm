9a18b3122e0a92ca68631816e8fb26b667942258
Wonjohn Choi <wonjohn.choi@gmail.com>
2012-02-18 06:37:32 -0800

    fixed cooldown limit

--- game/gameMain.js
+++ game/gameMain.js
@@ -56,6 +56,7 @@ module.exports = function(app) {
               hp       : ships[i].hp,
               blink: damaged,
               bulletRadius : ships[i].bulletRadius,
+
           });
       }
     }
